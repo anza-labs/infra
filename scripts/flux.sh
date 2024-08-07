6c84a664f432738dae72114d4b185b469ea9ceda
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-08-07 14:00:36 +0200

    feat: refactor resources
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- scripts/flux.sh
+++ scripts/flux.sh
@@ -4,6 +4,11 @@ set -e
 set -o pipefail
 
 X_NAME="${1:-kubernetes}"
+BW_TOKEN="${2}"
+
+kubectl create secret generic bitwarden \
+    --namespace=kube-system \
+    --from-literal=token="${BW_TOKEN}"
 
 flux bootstrap github \
     --token-auth \
