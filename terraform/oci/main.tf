c780df3749c938d76830c708c7ccd890f7a881b4
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-08-22 14:07:00 +0200

    fix: variables in oci
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- terraform/oci/main.tf
+++ terraform/oci/main.tf
@@ -12,9 +12,6 @@ module "oci_core" {
   instance_name = "BdxPwD"
 
   tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key  = var.private_key
 }
 
 module "oci_a1_flex" {
--- terraform/oci/modules/core/core.tf
+++ terraform/oci/modules/core/core.tf
@@ -1,11 +1,3 @@
-provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key  = var.private_key
-  region       = var.region
-}
-
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
   ad_number      = var.availability_domain
