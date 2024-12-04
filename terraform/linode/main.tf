b44e1d1caf453077e010679417ad8bc9b2a5347a
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-12-04 12:26:04 +0100

    feat: add new cluster
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- terraform/linode/main.tf
+++ terraform/linode/main.tf
@@ -12,3 +12,16 @@ module "lke" {
   github_token    = var.github_token
   bitwarden_token = var.bitwarden_token
 }
+
+module "eu-1" {
+  source = "./modules/lke"
+  root   = abspath(path.module)
+
+  cluster_name = "eu-1"
+  k8s_version  = "latest"
+
+  flux            = true
+  github_repo     = "https://github.com/anza-labs/manifests"
+  github_token    = var.github_token
+  bitwarden_token = var.bitwarden_token
+}
