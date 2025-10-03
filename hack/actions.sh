ceb7c4b217a411f0b239e10d8df5e9d9db4bb96a
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-10-03 22:24:52 +0200

    feat: some changes
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- hack/actions.sh
+++ hack/actions.sh
@@ -1,9 +1,10 @@
+# shellcheck shell=bash
 retry() {
   local max_attempts=$1
   shift
   local attempt=1
 
-  while [ $attempt -le $max_attempts ]; do
+  while [ "$attempt" -le "$max_attempts" ]; do
     echo "Attempt $attempt of $max_attempts: $*"
 
     if "$@"; then
@@ -13,7 +14,7 @@ retry() {
       echo "Attempt $attempt failed."
       attempt=$((attempt + 1))
 
-      if [ $attempt -le $max_attempts ]; then
+      if [ "$attempt" -le "$max_attempts" ]; then
         echo "Retrying..."
       else
         echo "All $max_attempts attempts failed."
